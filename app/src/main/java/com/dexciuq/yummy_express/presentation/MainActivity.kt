a8d618debe2e4542f370101e4751977128bf12a0
dimok <zhasulanov04@list.ru>
2023-11-17 17:14:06 +0600

    Add bottom navigation bar

---
+++ app/src/main/java/com/dexciuq/yummy_express/presentation/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.dexciuq.yummy_express.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.dexciuq.yummy_express.R
+import com.dexciuq.yummy_express.databinding.ActivityMainBinding
+import com.dexciuq.yummy_express.presentation.utils.show
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setupBottomNavigationView()
+    }
+
+    private fun setupBottomNavigationView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(
+                R.id.nav_host_fragment_container
+            ) as NavHostFragment
+
+        navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+//                R.id.addressFragment, R.id.deliveryFragment -> {
+//                    binding.bottomNavigationView.hide(binding.navHostFragmentContainer)
+//                }
+
+                else -> {
+                    binding.bottomNavigationView.show(binding.navHostFragmentContainer)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dexciuq/yummy_express/presentation/utils/Extentions.kt
@@ -0,0 +1,33 @@
+package com.dexciuq.yummy_express.presentation.utils
+
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+fun BottomNavigationView.show(navHostFragmentContainer: View) {
+    if (visibility == View.VISIBLE) return
+    visibility = View.VISIBLE
+    val params = navHostFragmentContainer.layoutParams as ConstraintLayout.LayoutParams
+    params.setMargins(params.leftMargin, params.topMargin, params.rightMargin, params.bottomMargin)
+    navHostFragmentContainer.layoutParams = params
+}
+
+fun BottomNavigationView.hide(navHostFragmentContainer: View) {
+    if (visibility == View.GONE) return
+    visibility = View.GONE
+    val params = navHostFragmentContainer.layoutParams as ConstraintLayout.LayoutParams
+    params.setMargins(params.leftMargin, params.topMargin, params.rightMargin, 0)
+    navHostFragmentContainer.layoutParams = params
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
