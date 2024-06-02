ca20d2dddeb5957b0fea430add5b99e47aee7023
dimok <zhasulanov04@list.ru>
2024-06-02 13:20:02 +0500

    Update feat about-me

--- app/src/main/java/com/dexciuq/yummy_express/presentation/screen/profile/about_me/AboutMeFragment.kt
+++ app/src/main/java/com/dexciuq/yummy_express/presentation/screen/profile/about_me/AboutMeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.dexciuq.yummy_express.databinding.FragmentAboutMeBinding
 import com.dexciuq.yummy_express.presentation.screen.profile.ProfileViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,6 +26,9 @@ class AboutMeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
         viewModel.user.observe(viewLifecycleOwner) {
             binding.nameLayout.editText?.setText(it?.name)
             binding.surnameLayout.editText?.setText(it?.surname)
